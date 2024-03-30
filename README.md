39ac665895967011f0e535902445764f7b5b471c
evanrafuse <evanrafuse.er@gmail.com>
2024-03-30 16:10:22 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Chop N' Drop Form Template
+
+Project is a prototype form element using JS to be plugged into a WordPress site.
+
+### ToDo:
+- Feedback Element on Page
+- Math to calculate cost
+- Implement email functionality (just use client's default email app)
---
+++ main.js
@@ -0,0 +1,19 @@
+// window.open('mailto:test@example.com?subject=subject&body=body');
+
+function main() {
+    console.log("Initiate JS")
+    // object declarations
+    fname = document.getElementById("fname");
+    lname = document.getElementById("lname");
+    linfeet = document.getElementById("linfeet");
+    inCorner = document.getElementById("inCorner");
+    outCorner = document.getElementById("outCorner");
+    downspout = document.getElementById("downspout");
+    elbow = document.getElementById("elbow");
+    drop = document.getElementById("drop");
+    leafguard = document.getElementById("leafguard");
+    submitBtn = document.getElementById("submitBtn");
+    resetBtn = document.getElementById("resetBtn");
+}
+
+main();
