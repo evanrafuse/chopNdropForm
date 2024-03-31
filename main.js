dd7ee604a3a83e0f108fdc966685318aa715b866
evanrafuse <evanrafuse.er@gmail.com>
2024-03-31 19:30:21 -0300

    Math Started

--- main.js
+++ main.js
@@ -8,11 +8,25 @@ function resetForm() {
 }
 
 function reCalculate() {
-    console.log("Recalculating Total...")
+    console.log("Recalculating Total...");
+    linearFootageRpt.innerText = linearfeet + " ft of gutter - $" + (linearfeet*gutter10ftCost);
+    insideCornerRpt.innerText = insideCorners + " inside corners - $" + (insideCorners*insideCornerCost);
+    outsideCornerRpt.innerText = outsideCorners + " outside corners - $" + (outsideCorners*outsideCornerCost);
+    endCapRpt.innerText = endCaps + " end caps - $" + (endCapInt*endCapCost);
 }
 
 function main() {
     console.log("Initiate JS");
+
+    // Cost constants
+    gutter10ftCost = 5.00
+    insideCornerCost = 2.00
+    outsideCornerCost = 2.00
+    endCapCost = 1.00
+    downspoutCost = 5.00
+    elbowCost = 2.00
+    dropCost = 2.00
+    leafguardCost = 5.00
 
     // variable refs
     firstName = ""
@@ -20,23 +34,35 @@ function main() {
     linearfeet = 0
     insideCorners = 0
     outsideCorners = 0
+    endCaps = 0
     downSpouts = 0
     elbows = 0
     drops = 0
     leafguard = false
 
-    // doc refs
+    // ~~ doc refs ~~
+    // form elements
     fnameTxt = document.getElementById("fname");
     lnameTxt = document.getElementById("lname");
     linfeetInt = document.getElementById("linfeet");
     inCornerInt = document.getElementById("inCorner");
     outCornerInt = document.getElementById("outCorner");
+    endCapInt = document.getElementById("endCap");
     downspoutInt = document.getElementById("downspout");
     elbowInt = document.getElementById("elbow");
     dropInt = document.getElementById("drop");
     leafguardBool = document.getElementById("leafguard");
     submitBtn = document.getElementById("submitBtn");
     resetBtn = document.getElementById("resetBtn");
+    // report elements
+    linearFootageRpt = document.getElementById("linearFootageRpt");
+    insideCornerRpt = document.getElementById("insideCornerRpt");
+    outsideCornerRpt = document.getElementById("outsideCornerRpt");
+    endCapRpt = document.getElementById("endCapRpt");
+    downSpoutRpt = document.getElementById("downSpoutRpt");
+    elbowRpt = document.getElementById("elbowRpt");
+    dropRpt = document.getElementById("dropRpt");
+    leafguardRpt = document.getElementById("leafguardRpt");
 
     fnameTxt.addEventListener("input", () => {
         firstName = fname.value;
@@ -54,6 +80,10 @@ function main() {
     });
     outCornerInt.addEventListener("input", () => {
         outsideCorners = outCornerInt.value;
+        reCalculate();
+    });
+    endCapInt.addEventListener("input", () => {
+        endCaps = endCapInt.value;
         reCalculate();
     });
     downspoutInt.addEventListener("input", () => {
